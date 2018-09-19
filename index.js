b47e2cefa785c5384b682ca9de7c0d148a6661ab
Cevher <cevherkarakoc@live.com>
2018-09-19 14:02:22 +0300

    ADD Texture Loader

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ exports.render = require('./src/render');
 exports.createShader = require('./src/createShader');
 exports.createShaderProgram = require('./src/createShaderProgram');
 exports.createMesh = require('./src/createMesh');
+
+exports.loadImage = require('./src/texture/loadImage');
+exports.createTexture = require('./src/texture/createTexture');
---
+++ src/texture/createTexture.js
@@ -0,0 +1,21 @@
+const createTexture = (gl, image, mipmap = false) => {
+  const texture = gl.createTexture();
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texImage2D(gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    image
+  );
+
+  if (mipmap) gl.generateMipmap(gl.TEXTURE_2D);
+
+  // Texture Parameters ???
+
+  return texture;
+}
+
+module.exports = createTexture;
