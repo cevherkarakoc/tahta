332d089b06109376e7ecdc67fd82ac9865da50bd
Cevher <cevherkarakoc@live.com>
2018-09-19 14:03:39 +0300

    ADD manual test for texture

--- test/manual/manual_test.js
+++ test/manual/manual_test.js
@@ -1,6 +1,6 @@
 import { Matrix } from "webgl-math";
 
-import { render, createShader, createShaderProgram, createMesh } from "../../index.js"
+import { render, createShader, createShaderProgram, createMesh, loadImage, createTexture } from "../../index.js"
 
 const glMatrix = require('./gl-matrix-min.js');
 
@@ -11,30 +11,52 @@ import { determinant } from "gl-matrix/src/gl-matrix/mat4";
 const canvas = document.querySelector("#webgl-canvas");
 const gl = canvas.getContext("webgl");
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+gl.enable(gl.BLEND);
 
 const vertex = createShader(gl, gl.VERTEX_SHADER, vsSource);
 const fragment = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
 const sp = createShaderProgram(gl, vertex, fragment);
 
 const vertices = new Float32Array([
-  -1.0, 1.0,
-  1.0, 1.0,
-  -1.0, -1.0,
-  1.0, -1.0,
+  -1.0, -1.0, 0.0,
+  1.0, -1.0, 0.0,
+  1.0, 1.0, 0.0,
+  -1.0, 1.0, 0.0
 ]);
-const indices = new Uint16Array([0, 1, 3, 0, 3, 2]);
+const indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
 
-const square = createMesh(gl, vertices, indices);
+const texCoord = new Float32Array([
+  0.0, 1.0,
+  1.0, 1.0,
+  1.0, 0.0,
+  0.0, 0.0,
+]);
+
+const square = createMesh(gl, vertices, indices, texCoord);
 
 const meshList = [
   square
-]
+];
+
+
+const textureList = [];
+loadImage("./textureA.png")
+  .then(image => {
+    textureList.push(createTexture(gl, image, true));
+    return loadImage("./textureB.png");
+  })
+  .then( image => {
+    textureList.push(createTexture(gl, image, true));
+  })
+  ;
 
 const { Transform, Camera } = Matrix;
 
 let projectionMatrix = Camera.perspective(Math.PI / 4, 1, 0.1, 100);
 //let projectionMatrix = Camera.ortho(-10,10, -10, 10, 1, 100);
-let viewMatrix = Transform.translate(Matrix.idendity(4), [0, 0, 0]);
+
+let viewMatrix = Camera.lookAt([0, 0, 20], [0, 0, -1], [0, 1, 0]);
 
 let modelMatrix = Matrix.idendity(4);
 
@@ -48,6 +70,16 @@ const programUniformList = [
     fn: (v, l) => gl.uniformMatrix4fv(l, false, v),
     value: Matrix.out(viewMatrix),
     location: gl.getUniformLocation(sp, 'uViewMatrix')
+  },
+  {
+    fn: (v, l) => gl.uniform1i(l, v),
+    value: 0,
+    location: gl.getUniformLocation(sp, 'uTexture0')
+  },
+  {
+    fn: (v, l) => gl.uniform1i(l, v),
+    value: 1,
+    location: gl.getUniformLocation(sp, 'uTexture1')
   }
 ]
 
@@ -59,21 +91,21 @@ const meshUniformList = [
       location: gl.getUniformLocation(sp, 'uModelMatrix')
     }
   ]
-]
+];
 
 let angle = 0;
 
 let scale = [4, 2, 1];
-let pos = [0, 0, -16];
+let pos = [0, 0, 0];
 let rot = [0, Math.PI / 4, Math.PI / 8];
 
 const loop = (timestamp) => {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  angle += 0.01;
+  angle += 0.05;
 
   rot[0] = angle;
-  rot[1] = angle * 2;
-  rot[2] = angle * 3;
+  rot[1] = 0;
+  rot[2] = angle* 0.2;
 
   let scaleMatrix = Transform.scale(Matrix.idendity(4), scale);
 
@@ -87,7 +119,7 @@ const loop = (timestamp) => {
 
   meshUniformList[0][0].value = Matrix.out(modelMatrix);
 
-  render(gl, sp, meshList, programUniformList, meshUniformList);
+  render(gl, sp, meshList, programUniformList, meshUniformList, textureList);
 
   window.requestAnimationFrame(loop);
 }
--- test/manual/shaders/basic.vert
+++ test/manual/shaders/basic.vert
@@ -1,11 +1,14 @@
 precision mediump float;
 
-attribute vec2 aPosition;
+attribute vec3 aPosition;
+attribute vec2 aTexCoord;
 
 uniform mat4 uProjectionMatrix;
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 
+varying vec2 vTexCoord;
 void main() {
-  gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 0.0, 1.0);
+  gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
+  vTexCoord = aTexCoord;
 }
